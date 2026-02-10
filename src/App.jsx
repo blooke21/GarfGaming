62f1e40241a37c1dcec54bd1438b6aa6c66fd2a0
Blake Warnock <blakewarn21@gmail.com>
2026-02-09 18:04:02 -0600

    initial commit
    sets up react app

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="app">
+      <header>
+        <h1>GarfGaming</h1>
+      </header>
+
+      <main>
+      </main>
+
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
