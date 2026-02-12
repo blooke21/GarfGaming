642433762b41e1976e6225720a7328b4d53b1562
mjweisse77 <mjweisse@yahoo.com>
2026-02-12 00:14:59 -0600

    first commit test + no gaming on Mondays

--- src/App.jsx
+++ src/App.jsx
@@ -17,23 +17,37 @@ export default function App() {
   
   console.log("new day", newDay)
 
-  return (
-    <div className="app">
-      <header>
-        <h1>GarfGaming</h1>
-        <h3>
-          Building a better gaming experience for garfield lovers around the
-          world
-        </h3>
-      </header>
-
-      <main>
-      <div>
-        <GameZone />
+  // prevent gaming on mondays
+  if (date.getDay() === 1) {
+    return (
+      <div className="app">
+        <div className="monday-message">
+          <img 
+            src="https://scontent-ord5-3.xx.fbcdn.net/v/t39.30808-6/468748758_10161216971540847_8617753216210066912_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0b6b33&_nc_ohc=BBhXy3XooMwQ7kNvwEdMJQG&_nc_oc=Adnc6-1UfAAywmvYwfiw0KvQtQKYP957ZgPMdVzHGjUxOgRwxIxBhQsS_AaUu8vEHjwLo_mbx_bkmn9ZLk2cVrNl&_nc_zt=23&_nc_ht=scontent-ord5-3.xx&_nc_gid=rFCan-biWp8xmwh-m_OInw&oh=00_AfttLOUKtBxxh-eHBmxzyCGedMZ-w7PQGr9QcVCiOzSOYQ&oe=69935093" 
+            alt="Monday message" 
+          />
+        </div>
       </div>
-      </main>
+    );
+  } else {
+    return (
+      <div className="app">
+        <header>
+          <h1>GarfGaming</h1>
+          <h3>
+            Building a better gaming experience for garfield lovers around the
+            world
+          </h3>
+        </header>
 
-      <footer></footer>
-    </div>
-  );
+        <main>
+        <div>
+          <GameZone />
+        </div>
+        </main>
+
+        <footer></footer>
+      </div>
+    );
+  }
 }
